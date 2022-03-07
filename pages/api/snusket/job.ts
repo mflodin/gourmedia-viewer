a0e7a3caac2a9fd21c6b626d9e00c5486b1d7bf9
Sebastian Ross <sebastian.ross@svt.se>
2022-03-07 20:19:05 +0100

    Try now

--- pages/api/snusket/job.ts
+++ pages/api/snusket/job.ts
@@ -1,10 +1,20 @@
-import puppeteer from "puppeteer";
 import Redis from "ioredis";
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Browser } from "puppeteer-core";
 
-let redisClient = new Redis(
-    process.env.REDIS_URL
-);
+let redisClient = new Redis(process.env.REDIS_URL);
+
+let chrome = {} as any;
+let puppeteer: any;
+
+if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+  // running on the Vercel platform.
+  chrome = require("chrome-aws-lambda");
+  puppeteer = require("puppeteer-core");
+} else {
+  // running locally.
+  puppeteer = require("puppeteer");
+}
 
 const DAYS = ["Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag"];
 const SECRET_KEY = "NONONO";
@@ -15,59 +25,65 @@ export default async function handler(
 ) {
   const auth = req.headers.authorization;
 
-//   if (process.env.NODE_ENV === "development" || auth === SECRET_KEY) {
-    try {
-      console.log("Fetching...");
-      const browser = await puppeteer.launch();
-      const page = await browser.newPage();
-      const navigationPromise = page.waitForNavigation();
+  //   if (process.env.NODE_ENV === "development" || auth === SECRET_KEY) {
+  try {
+    console.log("Fetching...");
+    const browser = await (process.env.AWS_LAMBDA_FUNCTION_VERSION ? puppeteer.launch({
+        args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+        defaultViewport: chrome.defaultViewport,
+        executablePath: await chrome.executablePath,
+        headless: true,
+        ignoreHTTPSErrors: true,
+      }) : puppeteer.launch()) as Browser;
+    const page = await browser.newPage();
+    const navigationPromise = page.waitForNavigation();
 
-      await page.goto(
-        "https://www.iss-menyer.se/restaurants/restaurang-gourmedia"
-      );
-      await page.setViewport({ width: 1440, height: 744 });
-      await navigationPromise;
+    await page.goto(
+      "https://www.iss-menyer.se/restaurants/restaurang-gourmedia"
+    );
+    await page.setViewport({ width: 1440, height: 744 });
+    await navigationPromise;
 
-      await page.waitForFunction(
-        () =>
-          (
-            document
-              .querySelector(
-                "div[data-mesh-id=comp-ko47p7a8inlineContent-gridContainer]"
-              )
-              ?.querySelector("textarea")?.value || ""
-          ).length > 0
-      );
+    await page.waitForFunction(
+      () =>
+        (
+          document
+            .querySelector(
+              "div[data-mesh-id=comp-ko47p7a8inlineContent-gridContainer]"
+            )
+            ?.querySelector("textarea")?.value || ""
+        ).length > 0
+    );
 
-      console.log("Parsing...");
-      let menus = await page.$$eval(
-        "div[data-mesh-id=comp-ko47p7a8inlineContent-gridContainer] div[class=_1ncY2]",
-        (divs) => divs.map((day) => day.querySelector("textarea")?.value)
-      );
-      console.log("DONE!");
-      await browser.close();
+    console.log("Parsing...");
+    let menus = await page.$$eval(
+      "div[data-mesh-id=comp-ko47p7a8inlineContent-gridContainer] div[class=_1ncY2]",
+      (divs) => divs.map((day) => day.querySelector("textarea")?.value)
+    );
+    console.log("DONE!");
+    await browser.close();
 
-      redisClient.set(
-        "week_menu",
-        JSON.stringify(
-          menus.map((menu, idx) => ({
-            day: DAYS[Math.min(idx, DAYS.length - 1)],
-            menu: menu || "",
-          }))
-        )
-      );
-      res.status(200).send("OK!")
-      //   res.status(200).json(
+    redisClient.set(
+      "week_menu",
+      JSON.stringify(
+        menus.map((menu, idx) => ({
+          day: DAYS[Math.min(idx, DAYS.length - 1)],
+          menu: menu || "",
+        }))
+      )
+    );
+    res.status(200).send("OK!");
+    //   res.status(200).json(
     //   menus.map((menu, idx) => ({
     //     day: DAYS[Math.min(idx, DAYS.length - 1)],
     //     menu: menu || "",
     //   }));
-      //   );
-    } catch (err: any) {
-      console.log("Error", err);
-      res.status(500).send(err.message);
-    }
-//   } else {
-//     return res.status(401).end();
-//   }
+    //   );
+  } catch (err: any) {
+    console.log("Error", err);
+    res.status(500).send(err.message);
+  }
+  //   } else {
+  //     return res.status(401).end();
+  //   }
 }
