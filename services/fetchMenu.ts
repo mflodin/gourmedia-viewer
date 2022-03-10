236eba9c1fcf458f9d7a687b1c7e2ccdacf5b578
Sebastian Ross <sebastian.ross@svt.se>
2022-03-10 20:22:44 +0100

    Fix env vars

--- services/fetchMenu.ts
+++ services/fetchMenu.ts
@@ -1,2 +1,2 @@
-const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL || process.env.BASE_URL || "http://localhost:3000";
+const BASE_URL = process.env.VERCEL_URL || "http://localhost:3000";
 export const fetchMenu = async () => (await fetch(`${BASE_URL}/api/snusket/today`)).json();
