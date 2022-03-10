acd6d54f0e41c03333b7686205d2cb83a043f69d
Sebastian Ross <sebastian.ross@svt.se>
2022-03-10 20:27:51 +0100

    Ok now?

--- services/fetchMenu.ts
+++ services/fetchMenu.ts
@@ -1,2 +1,2 @@
-const BASE_URL = process.env.VERCEL_URL || "http://localhost:3000";
+const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
 export const fetchMenu = async () => (await fetch(`${BASE_URL}/api/snusket/today`)).json();
