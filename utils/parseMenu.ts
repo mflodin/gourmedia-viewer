896bcb07d37650e6c81e3fb4ba224044fa545142
Martin Flodin <mflodin@gmail.com>
2025-05-24 01:30:32 +0200

    Fix for serializer
    - Use null instead of undefined

--- utils/parseMenu.ts
+++ utils/parseMenu.ts
@@ -9,28 +9,29 @@ export default function parseMenu(weekMenu: RedisWeekMenu): WeekMenu {
   const today = getDay(new Date()) - 1;
 
   return {
-    week: weekMenu.week,
-    year: weekMenu.year,
-    dayMenus: weekMenu.menuItems.map((dayMenu, idx) => {
-      const courses = dayMenu.menu
-        ?.split(/\n(.*)\t|\n/)
-        .filter((arr) => arr.trim());
-      return {
-        day: dayMenu.day,
-        isPast: idx < today,
-        isToday: today === idx,
-        courses: courses.map((course, courseIdx) => {
-          let [header, description] = course.trim().split(/\t|\n|\s{3,}/);
-          if (!description) {
-            description = header;
-            header = `Maträtt ${courseIdx + 1}`;
-          }
-          return {
-            type: header ?? "",
-            dish: fixPunctuation(description) ?? "",
-          };
-        }),
-      };
-    }),
+    week: weekMenu.week ?? null,
+    year: weekMenu.year ?? null,
+    dayMenus:
+      weekMenu?.menuItems?.map((dayMenu, idx) => {
+        const courses = dayMenu.menu
+          ?.split(/\n(.*)\t|\n/)
+          .filter((arr) => arr.trim());
+        return {
+          day: dayMenu.day,
+          isPast: idx < today,
+          isToday: today === idx,
+          courses: courses.map((course, courseIdx) => {
+            let [header, description] = course.trim().split(/\t|\n|\s{3,}/);
+            if (!description) {
+              description = header;
+              header = `Maträtt ${courseIdx + 1}`;
+            }
+            return {
+              type: header ?? "",
+              dish: fixPunctuation(description) ?? "",
+            };
+          }),
+        };
+      }) ?? [],
   };
 }
